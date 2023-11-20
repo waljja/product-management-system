715368798c556501b5e3765ce329bf6ee7d3ea39
waljja <2576723618@qq.com>
2023-11-20 11:37:14 +0800

    出货看板CK00客户TO单数量/4，装箱数/卡板数/出货数量统计加上条件确保数据准确

--- product-kanban-api/src/main/java/com/example/productkanbanapi/service/impl/KanbanServiceImpl.java
+++ product-kanban-api/src/main/java/com/example/productkanbanapi/service/impl/KanbanServiceImpl.java
@@ -11,8 +11,6 @@ import com.example.productkanbanapi.service.KanbanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 /**
  * KanbanServiceImpl
  *
